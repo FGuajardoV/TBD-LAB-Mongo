a8b14f551fb2b7dde48dcaf52eaa5d007c0f41f6
Francisco <francisco.guajardo.v@usach.cl>
2021-01-27 12:10:49 -0300

    Fix

--- src/main/java/voluntariado/demo/repositoriesMongo/TaskRepository.java
+++ src/main/java/voluntariado/demo/repositoriesMongo/TaskRepository.java
@@ -1,1 +1,13 @@
 package voluntariado.demo.RepositoriesMongo;
+import voluntariado.demo.Document.Task;
+
+import java.util.List;
+import org.bson.Document;
+
+
+public interface TaskRepositoryMongo {
+
+    public List<Task> getTask();
+    public Task createTarea(Task task);
+
+}
--- src/main/java/voluntariado/demo/repositoriesMongo/TaskRepositoryImp.java
+++ src/main/java/voluntariado/demo/repositoriesMongo/TaskRepositoryImp.java
@@ -1,1 +1,37 @@
 package voluntariado.demo.RepositoriesMongo;
+
+import voluntariado.demo.Document.Task;
+
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TaskRepositoryImpMongo implements TaskRepositoryMongo{
+
+    @Autowired
+    MongoDatabase database;
+
+    @Override
+    public List<Task> getTasks() {
+        MongoCollection<Task> collection = database.getCollection("Task", Task.class);
+        List <Task> emergencias = collection.find().into(new ArrayList<>());
+
+        return emergencias;
+    }
+
+    @Override
+    public Task createTask(Task task) {
+        MongoCollection<Task> collection = database.getCollection("task", Task.class);
+        collection.insertOne(task);
+        return task;
+    }
+
+}
